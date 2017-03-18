506e03c0a62983da81d89467a44559aa82833324
4nacs4 <4nacs4@gmailcom>
2017-03-17 21:06:10 -0400

    todoo

---
+++ dist/scripts/config.js
@@ -0,0 +1,13 @@
+var config = {};
+
+config.app = {
+    service: 'GC',
+};
+config.restApi = {
+    protocol: 'http',
+    port: '3000',
+    host: 'localhost',
+    apiPath: 'api',
+    authPath: 'auth'
+}
+
